002ecbfa5e70b78e74d22d27e10153e2ef852adb
IqbalHossain4 <iqbal-hossain@mail.com>
2023-05-19 14:24:27 +0600

    add my toys

--- src/pages/MyToys.jsx
+++ src/pages/MyToys.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
+import { useContext } from "react";
+import { AuthContext } from "../context/AuthProvider";
 
 const MyToys = () => {
-  return <div>hellos</div>;
+  const { user } = useContext(AuthContext);
+  const [toys, setToys] = useState([]);
+  useEffect(() => {
+    fetch(`http://localhost:5000/toy?email=${user?.email}`)
+      .then((res) => res.json())
+      .then((data) => setToys(data));
+  }, []);
+  console.log(toys);
+  return (
+    <div>
+      <h1>{toys.length}</h1>
+    </div>
+  );
 };
 
 export default MyToys;
